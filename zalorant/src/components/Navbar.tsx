a1341d7e3dda359e841a024ac7913efb7848a224
amala <amalananda332@gmail.com>
2024-11-19 23:31:12 +0700

    feat: create navbar

--- zalorant/src/components/Navbar.tsx
+++ zalorant/src/components/Navbar.tsx
@@ -1,20 +1,37 @@
-import Link from 'next/link'
-
+import Link from 'next/link';
+import Search from './Search'
 
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-white shadow-md">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl font-bold text-black">Zalorant</a>
       </div>
+      <Search />
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li><Link href='/'>Home</Link> </li>
-          <li><a>Products</a></li>
-          <li><a>Login</a></li>
-          <li><Link href='/register'>Register</Link> </li>
+        <ul className="menu menu-horizontal px-1 space-x-4">
+          <li>
+            <Link href="/" className="text-black font-medium hover:text-gray-500">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/products" className="text-black font-medium hover:text-gray-500">
+              Products
+            </Link>
+          </li>
+          <li>
+            <Link href="/login" className="text-black font-medium hover:text-gray-500">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link href="/register" className="text-black font-medium hover:text-gray-500">
+              Register
+            </Link>
+          </li>
         </ul>
       </div>
     </div>
-  )
+  );
 }
