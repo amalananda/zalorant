84d43885a91a23aeba6436615a9a05a54d4375e2
amala <amalananda332@gmail.com>
2024-11-20 22:59:15 +0700

    feat: create shuffle and product card

---
+++ zalorant/src/components/ProductCard.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { ProductType } from "@/types"
+import Image from "next/image"
+
+interface ProductCardProps {
+  product: ProductType
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  return (
+    <div className="border p-4 rounded-lg shadow-md hover:shadow-lg transition">
+      <Image
+        src={product.thumbnail || "https://i.pinimg.com/736x/d5/1c/15/d51c15ffa9cb570efeac761f12f14316.jpg"}
+        alt={product.name}
+        width={600}
+        height={600}
+        className="w-full h-48 object-cover rounded-lg mb-4"
+      />
+      <h3 className="text-xl font-semibold text-gray-800">{product.name}</h3>
+      <p className="text-gray-600 text-sm">{product.excerpt}</p>
+      <p className="text-lg font-bold text-indigo-600 mt-2">
+        Rp{product.price.toLocaleString("id-ID")}
+      </p>
+    </div>
+  )
+}
+
+export default ProductCard
